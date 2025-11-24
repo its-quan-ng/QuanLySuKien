a3dc590d5228426a64ea6723685bd0d880366db4
Nguyen Quan <ngquan6704@gmail.com>
2025-11-24 02:42:44 -0500

    improve UI home page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,15 +19,36 @@ namespace QuanLySuKien.Controllers
 
         public async Task<IActionResult> Index()
         {
-            // Lấy 6 sự kiện sắp diễn ra
-            var upcomingSuKiens = await _context.SuKiens
+            // Lấy events cho carousel (ưu tiên có ảnh)
+            var carouselEvents = await _context.SuKiens
                 .Include(s => s.DiaDiem)
+                .Include(s => s.LoaiVes)
+                .Where(s => !string.IsNullOrEmpty(s.AnhBia))
+                .OrderBy(s => Guid.NewGuid()) // Random
+                .Take(5)
+                .ToListAsync();
+
+            ViewBag.CarouselEvents = carouselEvents;
+
+            // Lấy categories từ ALL events với event count
+            var categoryCounts = await _context.SuKiens
+                .Where(s => !string.IsNullOrEmpty(s.LoaiSuKien))
+                .GroupBy(s => s.LoaiSuKien)
+                .Select(g => new { Category = g.Key, Count = g.Count() })
+                .ToListAsync();
+
+            ViewBag.CategoryCounts = categoryCounts;
+
+            // Lấy 6 sự kiện nổi bật
+            var featuredEvents = await _context.SuKiens
+                .Include(s => s.DiaDiem)
+                .Include(s => s.LoaiVes)
                 .Where(s => s.NgayToChuc >= DateOnly.FromDateTime(DateTime.Now))
                 .OrderBy(s => s.NgayToChuc)
                 .Take(6)
                 .ToListAsync();
 
-            return View(upcomingSuKiens);
+            return View(featuredEvents);
         }
 
         public IActionResult Privacy()
